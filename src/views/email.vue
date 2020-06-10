21572ddc575c681374bc028b8d317017da0019fe
jt2326 <2374797129@qq.com>
2020-06-10 20:38:07 +0800

    vue bus

--- src/views/email.vue
+++ src/views/email.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
+  <div class="email">
     email: jitong2326@163.com
+    <button @click="handleClick">点我</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'email',
+  methods: {
+    handleClick () {
+      this.$bus.$emit('on-click', 'hello')
+    }
+  }
 }
-
 </script>
+
 <style>
+.email{
+  border: 1px solid green;
+}
 </style>
--- src/views/tel.vue
+++ src/views/tel.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     tel: 17857589203
+    <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'tel',
+  data () {
+    return {
+      message: ''
+    }
+  },
+  mounted () {
+    this.$bus.$on('on-click', mes => {
+      this.message = mes
+    })
+  }
 }
 
 </script>
